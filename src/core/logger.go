d72a25c4f43a20960d69cdfcde9d6cb2d73746b3
van <vexyzy@yandex.ru>
2025-07-24 15:41:08 +0400

    feat: allow logs in the file

--- src/core/logger.go
+++ src/core/logger.go
@@ -1,13 +1,24 @@
 package core
 
 import (
+	"io"
 	"log/slog"
 	"os"
 )
 
 
 func GetConfigureLogger(level slog.Level) (*slog.Logger) {
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+	logFilePath := "logs.json"
+
+	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		// If opening the log file fails, panic as logging to file is a critical requirement
+		panic("Failed to open log file: " + err.Error())
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, file)
+
+	handler := slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{
 		Level: level,
 		AddSource: true,
 	})
